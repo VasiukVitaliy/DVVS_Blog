f0888fc10ed14760637aab5968ed9bde8d33635d
VasiukVitaliy <VITALII.VASIUK@lnu.edu.ua>
2025-12-01 14:02:26 +0200

    Fix import statements in Registration component for better navigation handling

--- frontend/src/Registration.jsx
+++ frontend/src/Registration.jsx
@@ -1,5 +1,7 @@
 import axios from "axios";
-import { useState } from "react";
+import { useState} from "react";
+
+import { useNavigate } from "react-router-dom";
 
 export default function RegisterForm() {
   const [nick, setNick] = useState("");
